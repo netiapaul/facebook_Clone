67f8863a8fa153ad97949e8ad9a6c96dc7908f53
netiapaul <netiapaul23@gmail.com>
2020-11-02 10:30:23 +0300

    added the login form on the login page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -9,7 +9,7 @@ const Login = ()=>{
             <div className="row">
                 <div className="col">
                     <img src={logo} className="img-fluid" alt="Responsive logo brand"/>
-                    <p>Facebook helps you connect and share <br/> with the people in yur life</p>
+                    <p className="brand-pr">Facebook helps you connect and share with the people in yur life</p>
                 </div>
 
                 {/* LOGIN FORM */}
@@ -18,16 +18,19 @@ const Login = ()=>{
                         <div className="card-body">
                         <form>
                             <div className="form-group">
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                <input type="email" className="form-control rounded" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                             </div>
 
                             <div className="form-group">
-                                <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                <input type="password" className="form-control rounded" id="exampleInputPassword1"/>
                             </div>
 
-                            <button type="button" className="btn btn-primary btn-lg btn-block">Log In</button>
+                            <button type="button" className="btn btn-primary btn-lg btn-block rounded">Log In</button>
                         </form>
+                            <p className="text-center mt-2">Forgotten password?</p>
                         </div>
+                        <hr className="mx-4"/>
+                        <button type="button" className="btn btn-success btn-lg mx-4 mb-4">Create New Account</button>
                     </div>
                 </div>
             </div>
