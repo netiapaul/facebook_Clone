d11d141f900737d83bf3ebd280e2acdf76ea4d03
netiapaul <netiapaul23@gmail.com>
2020-11-05 11:09:25 +0300

    added registration modal

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -41,22 +41,22 @@ const Login = ()=>{
                         <button type="button" className="btn btn-success btn-lg" data-toggle="modal" data-target="#exampleModal">Create New Account</button>
 
                         {/* MODAL OPENING */}
-                        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div className="modal-dialog-centered modal-dialog">
                                 <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div className="modal-body">
-                                    ...
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" className="btn btn-primary">Save changes</button>
-                                </div>
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="exampleModalLabel">Sign Up</h5>
+                                        {/* <p>It's quick and easy</p> */}
+                                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        
+                                        </button>
+                                    </div>
+                                    <div className="modal-body">
+                                        
+                                        {/* Register component Import */}
+                                        < Register />
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,9 +1,37 @@
 import React from 'react';
-// import './register.css';
+import './register.css';
 
 const Register = () =>{
     return(
-        <p>Modal body text goes here.</p>
+        <form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <input type="email" class="form-control" id="firstname" placeholder="First Name"/>
+    </div>
+
+    <div class="form-group col-md-6">
+      <input type="password" class="form-control" id="surname" placeholder="Surname"/>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <input type="email" class="form-control" id="email" placeholder="email address"/>
+  </div>
+
+  <div class="form-group">
+    <input type="password" class="form-control" id="password" placeholder="password"/>
+  </div>
+
+  <p className="register_note">
+  By clicking Sign Up, you agree to our Terms, Data Policy and Cookie Policy. You may receive SMS notifications from us and can opt out at any time.
+  </p>
+
+ <div className="text-center">
+    <button type="submit" class="btn btn-success">Sign Up</button>
+ </div>
+  
+
+</form>
     )
 };
 
