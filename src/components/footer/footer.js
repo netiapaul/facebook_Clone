82f586521f1404d68a521b2ba10aad7a08742e8d
netiapaul <netiapaul23@gmail.com>
2020-11-05 09:07:43 +0300

    added footer and corrected the spacing

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './footer.css';
 
 const Footer = () =>{
     return(
         <div class="jumbotron jumbotron-fluid  mb-0 mt-5" style={{backgroundColor:"white"}} >
             <div class="container">
+                <div className="row"></div>
+                <div className="row"></div>
                 <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
             </div>
         </div>       
