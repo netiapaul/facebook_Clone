8237cd492fd1ee2332cff256a8a841edad80c2df
netiapaul <netiapaul23@gmail.com>
2020-11-23 15:12:30 +0300

    added route path and the home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Login from './components/login/login.js';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'  
+import Home from './components/home/home';
 
 function App() {
   return (
-    <div className="App">
-     <Login />
-    </div>
+    <Router>
+      <React.Fragment>
+        <Route exact path='/' component={Login}/>
+        <Route path='/login' component={Login}/>
+        <Route exact path='/home' component={Home}/>
+        {/* <Login /> */}
+      </React.Fragment>    
+    </Router>
   );
 }
 
---
+++ src/components/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = ()=>{
+    return(
+        <div className="container-fluid">
+            <h1>I am Headed to greatness</h1>
+        </div>
+    )
+};
+
+export default Home;
